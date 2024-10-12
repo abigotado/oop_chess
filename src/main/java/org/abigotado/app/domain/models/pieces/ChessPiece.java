2162882db8b2fb27913dd999ca3684616aeb62ad
Nikita Kovalenko <nik.koval.89@gmail.com>
2024-10-12 15:25:27 -0300

    Create castling methods

--- src/main/java/org/abigotado/app/domain/models/pieces/ChessPiece.java
+++ src/main/java/org/abigotado/app/domain/models/pieces/ChessPiece.java
@@ -4,7 +4,7 @@ import org.abigotado.app.domain.models.ChessBoard;
 
 public abstract class ChessPiece {
      String color;
-     boolean check = true;
+     public boolean check = true;
 
     public ChessPiece(String color) {
         this.color = color;
